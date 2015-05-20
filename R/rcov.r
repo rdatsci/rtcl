2d1f940e1b715dbc66b716c3d8f736dd9a4ac4e3
Michel <michellang@gmail.com>
2015-05-20 11:34:51 +0200

    added covr support

---
+++ R/rcov.r
@@ -0,0 +1,22 @@
+#' Test the coverage of a package
+#'
+#' @description
+#' Uses the \code{covr} package (\url{https://github.com/jimhester/covr}) to
+#' test the code coverage.
+#'
+#' @template path
+#' @template return-itrue
+#' @export
+rcov = function(path = getwd()) {
+  pkg = devtools::as.package(path)
+  messagef("Checking code coverage of package '%s' ...", pkg$package)
+  lib = getLibraryPath()
+  found = length(find.package("covr", quiet = TRUE, lib = lib)) > 0L
+  if (!found) {
+    messagef("Installing missing GitHub package 'covr' ...")
+    rinstall("jimhester/covr")
+  }
+  requireNamespace("covr", quietly = TRUE)
+  print(covr::package_coverage(pkg))
+  invisible(TRUE)
+}
