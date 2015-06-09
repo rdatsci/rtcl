e3c8f0c9aa1093e052bbe21b259d88039c919c27
Michel <michellang@gmail.com>
2015-06-09 20:36:26 +0200

    added rvignettes

---
+++ R/rvignette.r
@@ -0,0 +1,14 @@
+#' Build vignettes
+#'
+#' @description
+#' Build vignettes of the package located in \code{path} using \code{\link[devtools]{build_vignette}}.
+#'
+#' @template path
+#' @template return-itrue
+#' @export
+rvignettes = function(path = getwd()) {
+  pkg = devtools::as.package(path)
+  messagef("Building vignettes for package '%s' ...", pkg$package)
+  devtools::build_vignettes(pkg)
+  invisible(TRUE)
+}
