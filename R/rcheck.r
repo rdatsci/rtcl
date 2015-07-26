45653b7965ff6ea3419d82663f28b9b295bcb04c
Michel <michellang@gmail.com>
2015-07-26 02:25:08 +0200

    added compile flags to rcheck

--- R/rcheck.r
+++ R/rcheck.r
@@ -9,6 +9,8 @@
 #' @template return-itrue
 #' @export
 rcheck = function(path = getwd(), nocleanup = FALSE) {
+  Sys.setenv(R_MAKEVARS_USER = system.file("Makevars-template", package = "rt"))
+  on.exit(Sys.unsetenv("R_MAKEVARS_USER"))
   pkg = devtools::as.package(path)
   now = strftime(Sys.time(), format = "%Y%m%d-%H%M%S")
   log.path = file.path(dirname(tempdir()), sprintf("rcheck-%s-%s", pkg$package, now))
