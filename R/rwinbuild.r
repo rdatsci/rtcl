9c7a81b72f1bf31be2b3574f83221e241520a395
Michel <michellang@gmail.com>
2015-07-28 22:17:10 +0200

    overwrite maintainer email adress

--- R/rwinbuild.r
+++ R/rwinbuild.r
@@ -13,6 +13,22 @@ rwinbuild = function(path = getwd(), devel = FALSE) {
   pkg = devtools::as.package(path)
   assertFlag(devel)
 
+  maintainer = getOption("rt.maintainer", NULL)
+  if (!is.null(maintainer)) {
+    desc = read.dcf(file.path(pkg$path, "DESCRIPTION"))
+    if ("Maintainer" %in% colnames(desc)) {
+      desc[1L, "Maintainer"] = maintainer
+    } else {
+      desc = cbind(desc, Maintainer = maintainer)
+    }
+
+    path = tempfile("tmp-package")
+    if (!dir.create(path, recursive = TRUE) || !file.copy(pkg$path, path, recursive = TRUE))
+      stop(sprintf("Unable to copy package to %s", path))
+    pkg = devtools::as.package(file.path(path, basename(pkg$path)))
+    write.dcf(desc, file = file.path(pkg$path, "DESCRIPTION"))
+  }
+
   messagef("Building and uploading package '%s' in '%s' to the winbuilder", pkg$package, pkg$path)
   devtools::document(pkg)
   devtools::build_win(pkg, version = ifelse(devel, "R-devel", "R-release"))
