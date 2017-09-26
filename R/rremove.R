dc67b93645edf229aa0d42e7e38956e3dffd6db1
Michel Lang <michellang@gmail.com>
2017-09-26 21:18:41 +0200

    added rremove

---
+++ R/rremove.R
@@ -0,0 +1,22 @@
+#' Remove R packages
+#'
+#' @description
+#' Removes (uninstalls) R packages.
+#'
+#' @param pkgs [\code{character}]\cr
+#'  Strings convertible by \code{\link{stringToPackage}}.
+#' @template return-itrue
+#' @export
+rremove = function(pkgs = character(0L)) {
+  lib = getLibraryPath()
+
+  for (pkg in pkgs) {
+    if (length(find.package(pkg, lib.loc = lib, quiet = TRUE)) == 0L) {
+      messagef("Package '%s' not found")
+    } else {
+      messagef("Removing package '%s' from '%s' ...", pkg, lib)
+      remove.packages(pkg, lib = lib)
+    }
+  }
+  invisible(TRUE)
+}
