a905ab66bffa7f3a5a6eeaf0563fd0931d8343d0
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2018-05-14 15:20:05 +0200

    add flag for submission to CRAN

--- R/rhub.r
+++ R/rhub.r
@@ -1,18 +1,25 @@
 #' Upload package to rhub
 #'
 #' @description
-#' Uploads a package located in \code{path} to the rhub service.
+#' Uploads a package located in \code{path} to the rhub service via \code{link[rhub]{check}}.
 #'
-#' @template path
 #' @param platform [\code{character(1)}]\cr
-#'  Check on the platform specified here.
+#'   Check on the platform specified here.
+#' @param checkforcran [\code{logical(1L)}]\cr
+#'   Use \code{link[rhub]{check_for_cran}} instead.
+#' @template path
 #' @template return-itrue
 #' @export
-rhub = function(path = getwd(), platform = NULL) {
+rhub = function(platform = NULL, checkforcran = FALSE, path = getwd()) {
   if (!requireNamespace("rhub"))
     stop("Install 'rhub' to use rhub")
+  assertFlag(checkforcran)
   pkg = devtools::as.package(path, create = FALSE)
   assertSubset(platform, rhub::platforms()$name)
-
-  rhub::check(pkg$path, platform = platform)
+  
+  if (checkforcran) {
+    rhub::check_for_cran(pkg$path, platform = platform)
+  } else {
+    rhub::check(pkg$path, platform = platform)
+  }
 }
