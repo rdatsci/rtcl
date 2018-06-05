4cc79fafff81a4df6821f7ff5ec0b76b0df60a6d
Michel Lang <michellang@gmail.com>
2018-06-05 14:32:33 +0200

    use install_git for temp packages

--- R/installPackage.r
+++ R/installPackage.r
@@ -20,23 +20,27 @@ installPackage.CranPackage = function(pkg, ...) {
 
 #' @export
 installPackage.GitPackage = function(pkg, temp = TRUE, force = FALSE, quick = TRUE, ...) {
-  path = if (temp) file.path(tempdir(), pkg$name) else file.path("~", ".rt", "git", pkg$name)
-  path = normalizePath(path, mustWork = FALSE)
-  pkg.path = if (is.na(pkg$subdir)) path else file.path(path, pkg$subdir)
   cli = getOption("rt.cli", FALSE)
-  if (!dir.exists(path)) {
-    messagef("Fetching and installing new git package '%s' ...", pkg$name)
-    dir.create(path, recursive = TRUE)
-    branch = if (!is.na(pkg$tag)) pkg$tag else NULL
-    git2r::clone(pkg$uri, local_path = path, progress = FALSE, branch = branch)
-    devtools::install(pkg.path, reload = !cli, quick = quick, keep_source = FALSE)
+  if (temp) {
+    pkg = stringToPackage("r-lib/withr")
+    devtools::install_git(stri_replace_first_regex(pkg$uri, "^https://", "git://"), reload = !cli, quick = quick, keep_source = FALSE)
   } else {
-    messagef("Updating git package '%s' ...", packageToString(pkg))
-    repo = git2r::repository(path)
-    fetched = git2r::fetch(repo, "origin")
-    if (force || fetched@total_objects > 0L) {
-      git2r::pull(repo)
-      devtools::install(pkg = pkg.path, reload = !cli, quick = quick, keep_source = FALSE)
+    path = normalizePath(file.path("~", ".rt", "git", pkg$name), mustWork = FALSE)
+    pkg.path = if (is.na(pkg$subdir)) path else file.path(path, pkg$subdir)
+    if (!dir.exists(path)) {
+      messagef("Fetching and installing new git package '%s' ...", pkg$name)
+      dir.create(path, recursive = TRUE)
+      branch = if (!is.na(pkg$tag)) pkg$tag else NULL
+      git2r::clone(pkg$uri, local_path = path, progress = FALSE, branch = branch)
+      devtools::install(pkg.path, reload = !cli, quick = quick, keep_source = FALSE)
+    } else {
+      messagef("Updating git package '%s' ...", packageToString(pkg))
+      repo = git2r::repository(path)
+      fetched = git2r::fetch(repo, "origin")
+      if (force || fetched@total_objects > 0L) {
+        git2r::pull(repo)
+        devtools::install(pkg = pkg.path, reload = !cli, quick = quick, keep_source = FALSE)
+      }
     }
   }
 }
