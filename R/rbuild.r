7363bfac617e19e618f0b0ce0f9e8766f09697ae
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2018-09-26 16:40:25 +0200

    update rbuild

--- R/rbuild.r
+++ R/rbuild.r
@@ -1,17 +1,13 @@
 #' Build a package
 #'
 #' @description
-#' Build a package located in \code{path} using \code{link[devtools]{build}}.
+#' Build a package located in \code{path} using \code{link[pkgbuild]{build}}.
 #'
 #' @template path
 #' @template return-itrue
 #' @export
 rbuild = function(path = getwd()) {
-  
-  # support of current CRAN and GitHub version needs loop way devtools
-  # use pkgbuild directly, when released on CRAN
-  # loc = pkgbuild::build(path)
-  loc = devtools::build(path)
+  loc = pkgbuild::build(path)
 
   messagef("The package has been bundled to '%s'.", normalizePath(loc))
   invisible(TRUE)
