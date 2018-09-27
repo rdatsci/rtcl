e91ebdc9ab3f26014dcbe65c4d2321f563ef6913
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2018-09-27 15:10:04 +0200

    rdoc

--- R/rdoc.r
+++ R/rdoc.r
@@ -1,15 +1,13 @@
 #' Create documentation for a package
 #'
 #' @description
-#' Documents a package located in \code{path} using \code{\link[roxygen2]{roxygen2-package}}
-#' or rather \code{link[devtools]{document}}.
+#' Documents a package located in \code{path} using \code{\link[roxygen2]{roxygen2-package}}.
 #'
 #' @template path
 #' @template return-itrue
 #' @export
 rdoc = function(path = getwd()) {
-  pkg = devtools::as.package(path, create = FALSE)
-  updatePackageAttributes(pkg)
-
+  updatePackageAttributes(path = path)
+  
   invisible(TRUE)
 }
