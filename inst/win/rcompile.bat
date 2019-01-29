69f22428ea719bd1f50bd6c269e8d1968d468c80
Jakob Richter <code@jakob-r.de>
2019-01-29 13:45:14 +0100

    use rknit for everything

--- inst/win/rcompile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: start help with "rcompile --help"
-
-SET args=%*
-Rscript %~dp0/../bin/rcompile %args%
--- inst/win/rmarkdown.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: start help with "rmarkdown --help"
-
-SET args=%*
-Rscript %~dp0/../bin/rmarkdown %args%
