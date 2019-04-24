fd2fd7209cae946743e44e4855f89b49652d0911
Jakob Richter <code@jakob-r.de>
2019-04-24 15:40:02 +0200

    try much more logic

--- R/rupdate.r
+++ R/rupdate.r
@@ -29,97 +29,19 @@ rupdate = function(rebuild = FALSE, neverupgrade = FALSE, savemode = FALSE) {
   assertFlag(savemode)
   upgrade = ifelse(neverupgrade, "never", "always")
 
-  messagef("Checking for outdated packages ...")
   lib = getLibraryPath()
   pkgs = getCollectionContents(as.packages = TRUE)
   pkgs_df = data.frame(
     rt_pkg = I(pkgs),
     rt_name = extract(pkgs, "name"),
-    rt_class = vapply(pkgs, function(x) head(class(x), 1), character(1L))
+    rt_class = vapply(pkgs, function(x) head(class(x), 1), character(1L)),
+    status = rep(NA_character_, length(pkgs))
   )
-  pkgs_installed = as.data.table(installed.packages())
-  pkgs_installed$meta = lapply(pkgs_installed$Package, getMeta)
-  pkgs_installed$meta_class = vapply(pkgs_installed$meta, getMetaType, character(1L))
-  pkgs_installed$status = "installed_local"
-  pkgs_df$Package = pkgs_df$rt_name
-  pkgs_df = merge(pkgs_df, pkgs_installed, all.x = TRUE, all.y = TRUE)
-  pkgs_df$status[is.na(pkgs_df$status)] = "requested_rt"
 
-  # Obtain package remote information
-  pkgs_deps = remotes::package_deps(as.character(pkgs_df$Package))
-
-
-  # Packages we want to install from CRAN:
-  # 1) New CRAN packages in rt file
-  # 2) If rebuilt: Packages that are build with an old R version
-  # 3) CRAN Packages with a new version available
-
-  # STEP: Rebuild cran packages, because we might not be able to update with broken packages
-
-  selector = !is.na(pkgs_df$meta_class) & pkgs_df$meta_class == "PackageCran" & pkgs_df$rebuild == TRUE & (pkgs_df$rt_class = "PackageCran" | is.na(pkgs_df$rt_class))
-
-  names_rebuild_cran = pkgs_df[selector, ]$Package
-  pkgs_df$status[selector] = "rebuilt_cran"
-  if (length(names_rebuild_cran) > 0) {
-    messagef("Rebuilding %i outdated packages from CRAN: %s", length(names_rebuild_cran), collapse(names_rebuild_cran))
-    # Do not use update because it does not rebuild (even with force)!
-    remotes::install_cran(as.character(names_rebuild_cran), lib = lib, force = TRUE, upgrade = upgrade)
+  x = list(done = FALSE, rebuild = rebuild, upgrade = upgrade, savemode = savemode, pkgs_df = pkgs_df, step = 0)
+  while (!res$done & x$step < 100) {
+    x = rupdate2(x)
   }
-
-  # STEP: update all non-cran packages in the collection for that we are not sure that they are up to date
-  selector = is.na(pkgs_df$meta_class) | pkgs_df$meta_class != "PackageCran" & pkgs_df$rebuild == TRUE
-  pkgs_df$rebuild = !(pkgs_df$rt_name %in% names_no_rebuild) & pkgs_df$rt_class != "PackageCran"
-  if (length(pkgs_df$rebuild) > 0) {
-    messagef("Adding %i packages to rebuild: %s", length(pkgs_df$rebuild),  collapse(pkgs_df[pkgs_df$rebuild == TRUE,]$rt_name))
-  }
-
-
-  # force installation for new packages and those that changed their type (eg. CRAN->GitHub)
-  pkgs_df$force_install = is.na(pkgs_df$meta_class) | (pkgs_df$meta_class != pkgs_df$rt_class) | pkgs_df$rebuild
-
-  if (any(pkgs_df$force_install)) {
-    messagef("Install %i new packages: %s", sum(pkgs_df$force_install), collapse(pkgs_df[pkgs_df$force_install == TRUE,]$rt_name))
-    rinstall(pkgs_df[pkgs_df$force_install == TRUE,]$rt_pkg, upgrade = upgrade, force = TRUE)
-  }
-
-
-  messagef("Auto update packages...")
-  pkgs_df$exclude_from_auto_update = pkgs_df$force_install == TRUE | pkgs_df$rt_class != "PackageCran"
-  pkgs_to_auto_update = setdiff(pkgs_installed_names, pkgs_df[pkgs_df$exclude_from_auto_update == TRUE, ]$rt_name)
-  if (!savemode) {
-    tryCatch({
-      remotes::update_packages(packages = pkgs_to_auto_update, upgrade = upgrade)
-    }, error = function(e) {
-      stop("remotes::update_packages failed with error:", "\n", as.character(e), "\n", "You can try to call rupdate with savemode.")
-    })
-  } else {
-    messagef("Update each package step by step in safemode...")
-    error_stack = list()
-    for (pkg_this in pkgs_to_auto_update) {
-      message("Package: ", pkg_this, appendLF = FALSE)
-      tryCatch({
-        remotes::update_packages(pkg_this, upgrade = upgrade)
-      }, error = function(e) {
-        er = as.character(e)
-        message(substr(er, 0, 25), "...", matchRegex(er, ".{1,25}$")[[1]], appendLF = FALSE)
-        error_stack <<- c(error_stack, list(list(package = pkg_this, error = er)))
-      })
-      message("")
-    }
-    if (length(error_stack)>0) {
-      stop("Failed with the following errors:", "\n", paste0(extract(error_stack, "package"), ": ", extract(error_stack, "error"), collapse = "\n"))
-    }
-  }
-
-
-  # install packages that have not been update by above line
-  # these are a) cran packages from the collection and b) all remote packages
-  pkgs_df$manual_update = !pkgs_df$force_install & pkgs_df$exclude_from_auto_update
-  if (any(pkgs_df$manual_update)) {
-    messagef("Update %i packages manually: %s", sum(pkgs_df$manual_update), collapse(pkgs_df[pkgs_df$manual_update == TRUE,]$rt_name))
-    rinstall(pkgs_df[pkgs_df$manual_update == TRUE,]$rt_pkg, upgrade = upgrade, force = FALSE)
-  }
-
   invisible(TRUE)
 }
 
@@ -153,4 +75,129 @@ getMetaType = function(meta) {
       return(NA_character_)
     }
   }
+}
+
+rupdate_result = function(x, pkgs_df, done = FALSE) {
+  x$pkgs_df = pkgs_df
+  x$done = done
+  x$step = x$step + 1
+  return(x)
+}
+
+merge_left_overwrites = function(x, y, by = "Package" , protect = "status") {
+  update_columns = setdiff(colnames(y), protect)
+  constant_columns = c(by, setdiff(colnames(x), update_columns))
+  merge(x = x[, constant_columns, drop = FALSE], y = y[, update_columns, drop = FALSE], all.x = TRUE, all.y = TRUE, by = by)
+}
+
+rupdate2 = function(x) {
+  # Collect information about r packages in rt config
+  messagef("Checking for outdated packages ...")
+
+  pkgs_df = x$pkgs_df
+
+  # ... and about installed r packages
+  pkgs_installed = as.data.table(installed.packages())
+  pkgs_installed$meta = lapply(pkgs_installed$Package, getMeta)
+  pkgs_installed$meta_class = vapply(pkgs_installed$meta, getMetaType, character(1L))
+  pkgs_df$Package = pkgs_df$rt_name
+  pkgs_df = merge_left_overwrites(pkgs_df, pkgs_installed)
+
+  selector = with(pkgs_df, !is.na(rt_class) & !is.na(meta_class) & meta_class != rt_class)
+  if (any(selector)) {
+    messagef("These %i packages: %s are installed in a different version then specified in the rt collection an will be removed (before they will be installed accordingly).", sum(selector), collapse(pkgs_df[selector]$Package))
+    remove.packages(pkgs = pkgs_df[selector]$Package)
+    pkgs_df$status[selector] = "removed_for_rt_update"
+    return(rupdate_result(x, pkgs_df))
+  }
+
+  # Obtain package remote information (cached because slow)
+  if (is.null(x$pkgs_deps) || !identical(x$pkgs_df$Package, pkgs_df$Package)) {
+    messagef("Obtaining remote version information for %i packages", nrow(pkgs_df))
+    pkgs_deps = as.data.frame(remotes::package_deps(as.character(pkgs_df$Package)))
+    colnames(pkgs_deps)[colnames(pkgs_deps) == "package"] = "Package"
+    x$pkgs_deps = pkgs_deps
+  }
+  pkgs_df = merge_left_overwrites(pkgs_df, pkgs_deps)
+
+
+  # Packages we want to install from CRAN:
+  # 1) New CRAN packages in rt file
+  # 2) If rebuild == TRUE: Packages that are build with an old R version
+  # 3) CRAN Packages with a new version available
+
+  # STEP: Rebuild cran packages, because we might not be able to update with broken packages
+
+  selector = with(pkgs_df, {
+    (is.na(meta_class) & !is.na(rt_class) & rt_class == "PackageCran") | #(1)
+    (x$rebuild & !is.na(meta_class) & meta_class == "PackageCran" & is.na(built) & built < getRversion()) | #(2)
+    (!is.na(meta_class) & meta_class == "PackageCran" & !is.na(status) & status != "updated" & !is.na(diff) & diff < 0) #(3)
+  })
+
+  if (any(selector)) {
+    messagef("Updating, re-/installing %i Packages from CRAN: %s", sum(selector), collapse(pkgs_df$Package[selector]))
+    # Do not use update because it does not rebuild (even with force)!
+    remotes::install_cran(as.character(pkgs_df[selector]$Package), lib = lib, force = TRUE, upgrade = x$upgrade)
+    pkgs_df$status[selector] = "updated"
+    return(rupdate_result(x, pkgs_df))
+  }
+
+  # Packages that we want to install from remotes
+  # 1) New remote packages in rt file
+  selector = with(pkgs_df, {
+    (is.na(meta_class) & !is.na(rt_class) & rt_class != "PackageCran") #(1)
+  })
+
+  if (any(selector)) {
+    messagef("Installing %i new packages from remotes: %s", sum(selector), collapse(pkgs_df$Package[selector]))
+    rinstall(pkgs_df$rt_pkg[selector], upgrade = x$upgrade, force = TRUE)
+    pkgs_df$status[selector] = "updated"
+    return(rupdate_result(x, pkgs_df))
+  }
+
+  # Packages that we want to install from remotes
+  # 2) If rebuild == TRUE: Packages with no version change that are build with an old R version but exist remotely
+  selector = with(pkgs_df, {
+    (x$rebuild & !is.na(meta_class) & meta_class != "PackageCran" & !is.na(status) & status != "updated" & is.na(built) & built < getRversion() & !is.na(diff) & diff == 0) #(2)
+  })
+
+  if (any(selector)) {
+    messagef("The following %i packages are built with an old R-Version and not from CRAN. They can only be updated if added to the rt collection at the time: %s", sum(selector), collapse(pkgs_df$Package[selector]))
+  }
+
+  # Packages that we can auto update
+  # 3) Installed remote packages with new version available
+  selector = with(pkgs_df, {
+    (!is.na(meta_class) & meta_class != "PackageCran" & !is.na(status) & status != "updated") #(2)
+  })
+
+  if (any(selector)) {
+    messagef("The following %i packages will be automatically updated: %s", sum(selector), collapse(pkgs_df$Package[selector]))
+    if (!x$savemode) {
+      tryCatch({
+        remotes::update_packages(packages = as.character(pkgs_df$Package[selector]), upgrade = x$upgrade)
+      }, error = function(e) {
+        stop("remotes::update_packages failed with error:", "\n", as.character(e), "\n", "You can try to call rupdate with savemode.")
+      })
+    } else {
+      messagef("Update each package step by step in safemode...")
+      error_stack = list()
+      for (pkg_this in as.character(pkgs_df$Package[selector])) {
+        message("Package: ", pkg_this, appendLF = FALSE)
+        tryCatch({
+          remotes::update_packages(pkg_this, upgrade = x$upgrade)
+        }, error = function(e) {
+          er = as.character(e)
+          message(substr(er, 0, 25), "...", matchRegex(er, ".{1,25}$")[[1]], appendLF = FALSE)
+          error_stack <<- c(error_stack, list(list(package = pkg_this, error = er)))
+        })
+        message("")
+      }
+      if (length(error_stack)>0) {
+        stop("Failed with the following errors:", "\n", paste0(extract(error_stack, "package"), ": ", extract(error_stack, "error"), collapse = "\n"))
+      }
+    }
+  }
+
+  return(return(rupdate_result(x, pkgs_df, TRUE)))
 }
