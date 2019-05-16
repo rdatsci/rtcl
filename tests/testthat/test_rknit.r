1613f81426ae14e1d0ae175df4c30480a0a47db0
Jakob Richter <code@jakob-r.de>
2019-05-16 16:39:45 +0200

    skip if pdflatex notavailable [skip travis]

--- tests/testthat/test_rknit.r
+++ tests/testthat/test_rknit.r
@@ -8,11 +8,17 @@ test_that("rknit works for various files", {
 
 
   for (knit_file in dir(files_path, full.names = TRUE)) {
-    expect_true(rknit(knit_file, clean = TRUE))
     knit_format = matchRegex(basename(knit_file), "(?<=\\.).+$")[[1]]
     out_base = matchRegex(basename(knit_file), ".+(?=\\.)")[[1]]
     out_format = matchRegex(out_base, "(?<=_).+$")[[1]]
     out_file = paste0(out_base,".",out_format)
+
+    if (out_format == "pdf" && !pkgbuild::has_latex()) {
+      skip("pdflatex not available")
+    }
+
+    expect_true(rknit(knit_file, clean = TRUE))
+
     expect_true(file.exists(out_file))
 
     # can we diverge from default?
