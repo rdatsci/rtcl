a25b3991e80999dc8652cdcbbee5cce8c112c8bd
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2019-06-27 15:46:35 +0200

    update rinstall

--- R/rinstall.r
+++ R/rinstall.r
@@ -5,7 +5,6 @@
 #'
 #' @param pkgs [\code{character}]\cr
 #'  Strings convertible by \code{\link{stringToPackage}}.
-#'  Default is the current directory: \dQuote{.}
 #' @param add [\code{logical(1)}]\cr
 #'  Append packages to your config file in \code{\link{getConfigPath}}.?
 #'  Default is \code{FALSE}.
@@ -13,25 +12,20 @@
 #'   Passed to \code{remotes::install_*()}.
 #' @template return-itrue
 #' @export
-rinstall = function(pkgs = ".", add = FALSE, ...) {
+rinstall = function(pkgs = character(0L), add = FALSE, ...) {
   pkgs = lapply(pkgs, stringToPackage)
   assertFlag(add)
-
-  # FIXME: Do we need to specify the lib_path?
-
   pn = extract(pkgs, "name")
   is.cran = vlapply(pkgs, inherits, "PackageCran")
   if (any(is.cran)) {
     messagef("Installing %i packages from CRAN: %s", sum(is.cran), collapse(pn[is.cran]))
-    remotes::install_cran(pn[is.cran], build_opts = getDefaultBuildOpts(remotes::install_cran, "cran"), ...)
+    remotes::install_cran(pn[is.cran], ...)
   }
-
-  dots = list(...)
+  
   for (pkg in pkgs[!is.cran]) {
-    dots$build_opts = readConfig()$build_opts[["remotes"]] #FIXME maybe this should be in the individial installPacakge functions
-    do.call(installPackage, c(list(pkg = pkg), dots))
+    installPackage(pkg, ...)
   }
-
+  
   if (add) {
     addPackagesToCollection(pkgs)
   }
