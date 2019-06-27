ac7d0c9ea5f59eaee0b25e60178530195305abcf
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2019-06-27 16:12:13 +0200

    try appveyor without trailing slashes

--- tests/testthat/test_rbuild.R
+++ tests/testthat/test_rbuild.R
@@ -12,11 +12,11 @@ test_basic_rbuild = function(s, name, ...) {
 
 test_that("rbuild works for local packages", {
   skip_on_cran()
-  test_basic_rbuild("./assets/package/")
+  test_basic_rbuild("./assets/package")
 })
 
 test_that("rbuild works for local packages on windows", {
   skip_on_os(c("mac", "linux", "solaris"))
   skip_on_cran()
-  test_basic_rbuild(".\\assets\\package\\", "testpkg")
+  test_basic_rbuild(".\\assets\\package", "testpkg")
 })
