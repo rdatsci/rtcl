b8dd7fc2e1c5e03082d4b83bc4e0c6e3544c9323
Jakob Richter <code@jakob-r.de>
2019-07-03 09:42:27 +0200

    error for debugging

--- R/stringToPackage.r
+++ R/stringToPackage.r
@@ -77,10 +77,10 @@ isPackageLocal = function(xs) {
   is.path = grepl(pattern = "^(\\/|\\.{1,2}\\/|~\\/|[A-Z]:/).+$", x = xs)
   is.file = grepl(pattern = ".*\\.(tar|zip|tar.gz|tar.bz2|tgz|tbz)$", x = xs)
   if (is.path && !dir.exists(xs)) {
-    warning(sprintf('Directory detected but %s does not exist.', xs))
+    stop(sprintf('Directory detected but %s does not exist.', xs))
   }
   if (is.file && !file.exists(xs)) {
-    warning(sprintf('File detected but %s does not exist.', xs))
+    stop(sprintf('File detected but %s does not exist.', xs))
   }
   (is.path && dir.exists(xs)) || (is.file && file.exists(xs))
 }
