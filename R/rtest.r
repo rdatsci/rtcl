7cde58381c893337645d4849f37f369c27e3fa87
Michel Lang <michellang@gmail.com>
2019-08-19 22:16:09 +0200

    support for tinytest

--- R/rtest.r
+++ R/rtest.r
@@ -12,17 +12,32 @@
 #' @template return-itrue
 #' @export
 rtest = function(path = getwd(), filter = NULL) {
+  assertString(filter, null.ok = TRUE)
+
   path = pkgload::pkg_path(path)
   pkgname = pkgload::pkg_name(path)
   updatePackageAttributes(path = path)
+  framework = getTestFramework(path)
 
-  messagef("Testing package '%s'", pkgname)
-  assertString(filter, null.ok = TRUE)
-  testpath = file.path(path, "tests", "testthat")
-  res = as.data.frame(testthat::test_dir(path = testpath, filter = coalesceString(filter)))
-  if (sum(res$failed) > 0) {
-    return(invisible(FALSE))
+  messagef("Testing package '%s' with %s", pkgname, framework)
+
+  if (framework == "testthat") {
+    testpath = file.path(path, "tests", "testthat")
+    res = as.data.frame(testthat::test_dir(path = testpath, filter = coalesceString(filter)))
+    ret = sum(res$failed) == 0L
   } else {
-    return(invisible(TRUE))
+    testpath = file.path(path, "inst", "tinytest")
+    res = tinytest::run_test_dir(testpath, pattern = coalesceString(filter, "^test.*\\.[rR]"))
+    ret = all(sapply(res, isTRUE))
   }
+
+  invisible(ret)
+}
+
+getTestFramework = function(path) {
+  suggests = read.dcf(file.path(path, "DESCRIPTION"), fields = "Suggests", keep.white = FALSE)
+  if (is.na(suggests))
+    return("testthat")
+  suggests = trimws(strsplit(suggests, split = ",", fixed = TRUE)[[1L]])
+  if ("tinytest" %in% suggests) "tinytest" else "testthat"
 }
