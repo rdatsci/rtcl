c8216d9c3a462085e94195b879a1e0136aabc5d2
Michel Lang <michellang@gmail.com>
2019-08-29 09:54:10 +0200

    fix path in rwinbuild

--- R/rwinbuild.r
+++ R/rwinbuild.r
@@ -2,7 +2,7 @@
 #'
 #' @description
 #' Uploads a package located in \code{path} to the winbuilder service.
-#' Uses \code{\link[devtools:check_win]{check_win_release}}, \code{\link[devtools:check_win]{check_win_devel}} 
+#' Uses \code{\link[devtools:check_win]{check_win_release}}, \code{\link[devtools:check_win]{check_win_devel}}
 #' or \code{\link[devtools:check_win]{check_win_oldrelease}} internally.
 #'
 #' @template path
@@ -13,22 +13,24 @@
 #' @template return-itrue
 #' @export
 rwinbuild = function(path = getwd(), devel = FALSE, oldrel = FALSE) {
-  path = normalizePath(path)
+  pkgname = pkgload::pkg_name(path = path)
+  pkgdir = pkgload::pkg_path(path = path)
+
   assertFlag(devel)
   assertFlag(oldrel)
   if (devel && oldrel) {
     stop("Just enable devel OR oldrel at the same time.")
   }
 
-  path = changeMaintainer(path)
+  pkgdir = changeMaintainer(pkgdir)
 
-  messagef("Building package in '%s' and uploading to winbuilder", path)
+  messagef("Building package in '%s' and uploading to winbuilder", pkgdir)
 
   if (devel) {
-    devtools::check_win_devel(path)
+    devtools::check_win_devel(pkgdir)
   } else if (oldrel) {
-    devtools::check_win_oldrelease(path)
+    devtools::check_win_oldrelease(pkgdir)
   } else {
-    devtools::check_win_release(path)
+    devtools::check_win_release(pkgdir)
   }
 }
